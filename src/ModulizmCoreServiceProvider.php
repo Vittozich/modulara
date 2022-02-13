4e7763ddd1e411596686e7c29d395b9632f3327f
Viktor Izotov <vittozich@gmail.com>
2022-02-13 19:20:30 +0300

    initial commit

---
+++ src/ModulizmCoreServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modulizm\Core;
+
+use Carbon\Laravel\ServiceProvider;
+
+class ModulizmCoreServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
